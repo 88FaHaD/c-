742704c1f5a9662ea5edcefafc6de5c7909a3129
FAHAD <95226468+88FaHaD@users.noreply.github.com>
2022-01-24 00:32:30 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,57 +1,54 @@
-#include <iostream>
+//Lab assignment Q4
+#include<iostream>
 
 using namespace std;
-
 int main()
 {
-    int N;
-    cout << "Number of days: ";
-    cin >> N;
-
-    int temperatures[N];
-    cout << "Enter " << N << " values (one per line/space seperated): " << endl;
-    for (int i = 0; i < N; i++)
-        cin >> temperatures[i];
-
-    cout << "------------------" << endl;
-
-    int hot_days_count = 0, pleasant_days_count = 0, cold_days_count = 0;
-    double sum = 0, average;
-
-    for (int i = 0; i < N; i++)
-    {
-        sum += temperatures[i];
-
-        cout << temperatures[i] << " F ---> ";
-
-        if (temperatures[i] >= 85)
-        {
-            cout << "Hot";
-            hot_days_count++;
-        }
-        else if (temperatures[i] >= 60 && temperatures[i] <= 84)
-        {
-            cout << "Pleasant";
-            pleasant_days_count++;
-        }
-        else if (temperatures[i] < 60)
-        {
-            cout << "Cold";
-            cold_days_count++;
-        }
-
-        cout << " day" << endl;
-    }
-
-    average = sum / N;
-    cout << "------------------" << endl;
-
-    cout << "Hot days: " << hot_days_count << endl;
-    cout << "Pleasant days: " << pleasant_days_count << endl;
-    cout << "Cold days: " << cold_days_count << endl;
-
-    cout << "------------------" << endl;
-    cout << "Average temperature: " << average << " F" << endl;
-
-    return 0;
+	int n,i;
+	cout<<"Enter the total No of days ";
+	cin>>n;
+	
+	int tempr[n];
+	cout<<"Enter various Temperatures ";
+	for(int i=0; i<n; i++)
+	   cin>>tempr[i];
+	
+	int Hot_days=0 ,Cold_days=0 ,Pleasent_days=0 ;
+	double sum=0,average;
+	cout<<endl;
+	for (int i=0; i<n ; i++)
+	{
+		sum+=tempr[i];
+		cout << tempr[i] <<"  ";
+	
+		
+		if(tempr[i] >= 85)
+		{
+			cout << "Hot day";
+		    Hot_days++;
+		}
+		
+		else if(tempr[i] <=84 && tempr[i] >=60)
+	    {
+	    		cout << "Pleasent day";
+		        Pleasent_days++;
+		}
+		
+		else if(tempr[i] < 60)
+	    {
+	    		cout <<"Cold day";
+		        Cold_days++;
+		}
+			cout<<endl;
+	}
+	cout<<endl;
+	cout<<"No. of Hot days are " <<Hot_days <<endl;
+	cout<<"No. of Cold days are " <<Cold_days <<endl;
+	cout<<"No. of Pleasent days are " <<Pleasent_days<<endl;
+	
+	cout<<endl;
+	average=sum/n;
+	cout<<"Average temperature is " <<average;
+	
 }
+
