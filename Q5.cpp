18d3581ab8c4c458430759f7d2e393c7df13b0f7
FAHAD <95226468+88FaHaD@users.noreply.github.com>
2022-02-13 14:40:31 +0530

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,16 @@
+// Q5  Which is generally more efficient, recursion or iteration?
+
+//A program is called recursive when an entity calls itself. A program is call iterative when there is a loop (or repetition).
+
+//Usage: Usage of either of these techniques is a trade-off between time complexity and size of code. If time complexity is the point of focus, and number of recursive calls would be 
+//large, it is better to use iteration. However, if time complexity is not an issue and shortness of code is, recursion would be the way to go.
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Q 5";
+}
+
---
+++ Q6.cpp
@@ -0,0 +1,34 @@
+// Q6 Which control statement do you typically find in a recursive function?
+
+//  IF ELSE control statment is  typically found in a recursive function.
+
+
+
+//A C++ control statement redirects the flow of a program in order to execute additional code. These statements come in the form of conditionals (if-else, switch) and loops
+// (for, while, do-while). Each of them relies on a logical condition that evaluates to a boolean value in order to run one piece of code over another.
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<" ABCD ";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
